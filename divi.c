6b599c99fec2bc3238bc8323a87453878d092b3b
Maria Eduarda do Amor Divino Dias Del Rei <dudadelrei10@gmail.com>
2025-10-04 17:24:57 -0300

    Mudanças

--- divi.c
+++ divi.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "operacoes.h"
 
-float div(float a, float b){
+float divi(float a, float b){
 	float c = a/b; 
 	return c; 
 } 
--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 //Analisar se eh digito ou operador
 #include <ctype.h>
+//Para malloc e calloc
 #include <stdlib.h>
+//Para pow
+#include <math.h>
 #include "operacoes.h"
 
 int main(void){
@@ -21,6 +24,9 @@ int main(void){
 	//Armazena memoria para os valores
 	float* valores = calloc(n, sizeof(float)); 
 
+	
+	//Checa se os ponteiros sao validos
+
 	//Armazena memoria para os operadores
 	char* operadores = malloc((n-1)*sizeof(char)); 
 
@@ -32,31 +38,59 @@ int main(void){
 
 	//Variaveis para controlar o tamanho dos vetores
 	int tmhval = 0, tmhop = 0; 
+	//Vetor para armazenar os digitos consecutivos obtidos
+	float* digitos = calloc(n, sizeof(float));
+	//Variavel para controlar o tamanho do vetor de digitos
+	int tmhdig = 0; 
+
 	//Verifica se eh digito ou nao e transforma o tipo caso seja
 	for (int i=0; i<(2*n-1); i++){
-		if(isdigit(entradas[i])){
-			valores[tmhval++] = entradas[i] - '0'; 
-		}else if(!isdigit(entradas[i])){
+		//Reseta a quantidade de digitos da entrada, se for digito
+		tmhdig = 0;
+		//Se nao for digito, armazena no vetor de operadores 
+		if(!isdigit(entradas[i])){
 			operadores[tmhop++] = entradas[i]; 
-		}
+		
+		}else{
+			//Enquanto for digito, armazena no vetor de digitos 
+			while(isdigit(entradas[i])){
+				//Transforma o char em float e armazena no vetor de digitos
+				digitos[tmhdig] = entradas[i] - '0';
+				tmhdig++; 
+				i++; 
+			} 
+			//Transforma o vetor de digitos em um unico numero 
+			for(int j=0; j<tmhdig; j++){
+				//Soma o valores dos digitos multiplicados por 10 elevado a posicao
+				valores[tmhval] += digitos[j]*pow(10, tmhdig-j-1);
+				//Aumenta o tamanho do vetor de valores apenas quando o numero esta completo
+				if(j==tmhdig-1){
+					tmhval++;
+				}
+			}
+		}	
 	}
 
 
 	float total = valores[0]; //Variavel para armazenar o resultado
 	//Analisa qual operador esta sendo pedido
-	for(int i=0; i<n-1; i++){
+	for(int i=0; i<n; i++){
 		switch(operadores[i]){
 			case '+': 
-				total = soma(total, valores[i]);
+				total = soma(total, valores[i+1]);
+				i++; 
 				break; 
 			case '-': 
-				total = sub(total, valores[i]);
+				total = sub(total, valores[i+1]);
+				i++; 
 				break;
 			case '/': 
-				total = divi(total, valores[i]);
+				total = divi(total, valores[i+1]);
+				i++; 
 				break;
 			case '*': 
-				total = mult(total, valores[i]);
+				total = mult(total, valores[i+1]);
+				i++; 
 				break;
 		}
 	}	
@@ -65,5 +99,6 @@ int main(void){
 
 	free(entradas); 
 	free(valores); 
-	free(operadores);git rn 0; 
+	free(operadores);
+	return 0; 
 }
