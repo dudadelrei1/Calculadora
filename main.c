09dac10216a1ee5ea49834033b3a311a81c7f9c9
Maria Eduarda do Amor Divino Dias Del Rei <dudadelrei10@gmail.com>
2025-10-04 19:23:38 -0300

    Mudarei o código consideravelmente, é bom deixar salvo o anterior

--- main.c
+++ main.c
@@ -3,8 +3,6 @@
 #include <ctype.h>
 //Para malloc e calloc
 #include <stdlib.h>
-//Para pow
-#include <math.h>
 #include "operacoes.h"
 
 int main(void){
@@ -23,8 +21,6 @@ int main(void){
 
 	//Armazena memoria para os valores
 	float* valores = calloc(n, sizeof(float)); 
-
-	
 	//Checa se os ponteiros sao validos
 
 	//Armazena memoria para os operadores
@@ -32,65 +28,53 @@ int main(void){
 
 	//Le os dados inseridos
 	printf("Insira os numeros e operadores (Ex: 2 + 3*4): \n"); 
-	for(int i=0; i<(2*n-1); i++){
-		scanf(" %c", &entradas[i]);
+	int tmhstr = n - 1; // quantidade de caracteres esperada (números + operadores)
+	for(int i = 0; i < tmhstr; i++){
+    	scanf(" %c", &entradas[i]);
+    	if(i > 0 && isdigit(entradas[i])){
+        	// Se dois dígitos seguidos formam um número, aumenta o tamanho esperado
+        	tmhstr++;
+    	}
 	}
 
 	//Variaveis para controlar o tamanho dos vetores
 	int tmhval = 0, tmhop = 0; 
-	//Vetor para armazenar os digitos consecutivos obtidos
-	float* digitos = calloc(n, sizeof(float));
+	//string para armazenar os digitos consecutivos obtidos
+	char* digitos = calloc(n, sizeof(char));
 	//Variavel para controlar o tamanho do vetor de digitos
 	int tmhdig = 0; 
-
+	
 	//Verifica se eh digito ou nao e transforma o tipo caso seja
-	for (int i=0; i<(2*n-1); i++){
-		//Reseta a quantidade de digitos da entrada, se for digito
-		tmhdig = 0;
-		//Se nao for digito, armazena no vetor de operadores 
-		if(!isdigit(entradas[i])){
-			operadores[tmhop++] = entradas[i]; 
-		
-		}else{
-			//Enquanto for digito, armazena no vetor de digitos 
-			while(isdigit(entradas[i])){
-				//Transforma o char em float e armazena no vetor de digitos
-				digitos[tmhdig] = entradas[i] - '0';
-				tmhdig++; 
-				i++; 
-			} 
-			//Transforma o vetor de digitos em um unico numero 
-			for(int j=0; j<tmhdig; j++){
-				//Soma o valores dos digitos multiplicados por 10 elevado a posicao
-				valores[tmhval] += digitos[j]*pow(10, tmhdig-j-1);
-				//Aumenta o tamanho do vetor de valores apenas quando o numero esta completo
-				if(j==tmhdig-1){
-					tmhval++;
-				}
-			}
-		}	
-	}
+	for (int i = 0; i < (2*n-1); i++) { 
+    	tmhdig = 0;
 
+    	if (!isdigit(entradas[i])) {
+        	operadores[tmhop++] = entradas[i]; 
+    	} else {
+        	while (i < (2*n-1) && isdigit(entradas[i])) {
+            	digitos[tmhdig++] = entradas[i++];
+        	}
+		i--; //Ajusta o indice pois o for incrementa ele
+        digitos[tmhdig] = '\0'; // fecha string
+        valores[tmhval++] = atof(digitos);//Converte string para float
+    }
+}
 
 	float total = valores[0]; //Variavel para armazenar o resultado
 	//Analisa qual operador esta sendo pedido
-	for(int i=0; i<n; i++){
+	for(int i=0; i<n-1; i++){
 		switch(operadores[i]){
 			case '+': 
 				total = soma(total, valores[i+1]);
-				i++; 
 				break; 
 			case '-': 
 				total = sub(total, valores[i+1]);
-				i++; 
 				break;
 			case '/': 
 				total = divi(total, valores[i+1]);
-				i++; 
 				break;
 			case '*': 
 				total = mult(total, valores[i+1]);
-				i++; 
 				break;
 		}
 	}	
@@ -100,5 +84,6 @@ int main(void){
 	free(entradas); 
 	free(valores); 
 	free(operadores);
+	free(digitos);
 	return 0; 
 }
