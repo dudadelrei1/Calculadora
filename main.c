8f088d5f645164b7fff90ecd3fc14c8c7246c509
Maria Eduarda do Amor Divino Dias Del Rei <dudadelrei10@gmail.com>
2025-10-04 21:09:57 -0300

    Subestimei a dificuldade de fazer esse codigo

--- main.c
+++ main.c
@@ -30,28 +30,49 @@ int main(void){
     	}
 	}
 
-	float total = valores[0]; //Variavel para armazenar o resultado
-	//Analisa qual operador esta sendo pedido
-	for(int i=0; i<n-1; i++){
-		switch(operadores[i]){
-			case '+': 
-				total = soma(total, valores[i+1]);
-				break; 
-			case '-': 
-				total = sub(total, valores[i+1]);
-				break;
-			case '/': 
-				total = divi(total, valores[i+1]);
-				break;
-			case '*': 
-				total = mult(total, valores[i+1]);
-				break;
-		}
-	}	
+	float total = 0; //Variavel para armazenar o resultado
+	int index = 0; 
+	int igualanmenosum = n-1; 
+	float resultado = 0; 
+	// Primeiro while (* e /)
+	while (index < igualanmenosum) {
+    	if (operadores[index] == '*' || operadores[index] == '/') {
+        	if (operadores[index] == '*'){
+            	valores[index] = mult(valores[index], valores[index+1]);
+        	}
+        	else{
+            	valores[index] = divi(valores[index], valores[index+1]);
+        	}
 
-	printf("O resultado final eh: %f", total); 
- 
-	free(valores); 
+        // Desloca os valores para a esquerda
+        for (int j = index+1; j < igualanmenosum; j++) {
+            valores[j] = valores[j+1]; 
+            operadores[j-1] = operadores[j];
+        }
+
+        igualanmenosum--; } // agora temos um número a menos
+        // não incrementa index, pois o novo valores[index] precisa ser reavaliado
+        index++;
+}
+
+	// Segundo while (+ e -)
+	index = 0; 
+	while (index < igualanmenosum) {
+    	if (operadores[index] == '+' || operadores[index] == '-') {
+        	if (operadores[index] == '+') {
+           		total = soma(valores[index], valores[index+1]);
+        } else {
+            	total = sub(valores[index], valores[index+1]);
+		}
+		
+        igualanmenosum--;
+    } 
+        index++;
+	}
+
+	printf("O resultado final eh: %f", total);
+
+	free(valores);
 	free(operadores);
 	return 0; 
 }
